0af5ea1dd35fd560c7ee2f8e091e4f6639e27672
merkis88 <axuenno88@gmail.com>
2025-10-23 01:23:29 +0700

    ADDED: create book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class BookController extends Controller
 {
+    public function store(Request $request): JsonResource
+    {
+        $validatedData = $request->validate([
+           'title' => 'required|string|max:255',
+           'author' => 'required|string|max:255',
+           'genre' => 'required|string|max:255',
+           'description' => 'nullable|string',
+        ]);
 
+        $createdBook = Book::create($validatedData);
+        return response()->json($createdBook, 201);
+    }
+
+    public function getAllBooks(): JsonResource
+    {
+        $books = Book::all();
+        return response()->json($books);
+
+    }
 }
